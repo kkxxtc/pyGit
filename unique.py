ec6fef9001b171f4e158185294386d10112955bf
kkxxtc <72883722+kkxxtc@users.noreply.github.com>
2020-10-22 17:10:33 +0300

    Delete unique.py

--- unique.py
+++
@@ -1,4 +0,0 @@
-with open('/Users/apple/Desktop/ Uniktext.txt') as book:
-    words = list(map(lambda x: x.strip('!?().,'),book.read().lower().split()))
-    let = dict(zip(words,map(lambda x:words.count(x),words)))
-    print(*sorted(let.items(),key = lambda x:x[1],reverse=True))
